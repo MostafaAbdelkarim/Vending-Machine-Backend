93169aec53534f34908949ee44093100521f0eed
Mostafa ElDahshan <mostafaabdelkarim22@gmail.com>
2022-12-24 14:06:07 +0200

    Added remote MongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
-module.exports = mongoose.connect('mongodb://localhost/VendingMachine')
+
+module.exports = mongoose.connect(config.get('RemoteDB'))
     .then(() => console.log('Connected to MongoDB Seccessfully.. '))
     .catch(err => console.error(`Cannot connect to MongoDB, Error: ${err}`));
-
-
